edd5a832ae0d6f6c3079ac71d2633e124c2c93e2
FabrizioGuidotti <fabrizio.guidotti.2000@gmail.com>
2022-03-17 13:16:15 +0100

    Added the snakemake implementation for the script wrap_features

--- code/wrap_features.R
+++ code/wrap_features.R
@@ -1,6 +1,39 @@
-library(tidyverse)
-library(GenomicRanges)
+# Parsing the provided arguments.
+#* This is done at the start of the file as the import of the required libraries below is really slow and 
+# we want to give the user the feedback immediately
+library("optparse")
+
+# The arguments
+option_list <- list(
+    make_option(c("-d", "--data"),
+        type = "character", default = NULL,
+        help = "The input dataset [Required]", metavar = "character"
+    ),
+    make_option(c("-o", "--out"),
+        type = "character", default = NULL,
+        help = "The output file name. The path is relative to the directory of the input. [Required]", metavar = "character"
+    )
+)
+
+# The argument parser
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+
+# Chacking that the input file has been passed
+if (is.null(opt$data) | is.null(opt$out)) {
+    stop("Please specify an input and an output file.")
+}
+
+# The input file name
+suppressMessages(library(crayon))
+message(blue("Started Execution of R Script."))
+
+# If no errors are found in the input parameters, proceed with the script.
+suppressMessages(library(tidyverse))
+suppressMessages(library(GenomicRanges))
 options(scipen = 999999999)
+suppressMessages(library(optparse))
+message(green("Successfully imported required libraries"))
 
 # the resolution datatable
 res_set <- c("1Mb", "500kb", "100kb", "50kb", "10kb", "5kb")
@@ -9,11 +42,11 @@ names(res_num) <- res_set
 
 #-----------------------------------------
 ## Utility function
-build_GRange_fn <- function(CAGE_file) {
+wrap_features_fn <- function(input_file) {
 
   # Load the data from the CAGE_file as an object called cage_coord_tbl
-  cage_coord_tbl <- get(base::load(CAGE_file))
-  tmp_obj <- names(mget(base::load(CAGE_file)))
+  cage_coord_tbl <- get(base::load(input_file))
+  tmp_obj <- names(mget(base::load(input_file)))
   rm(list = tmp_obj)
   rm(tmp_obj)
 
@@ -28,19 +61,23 @@ build_GRange_fn <- function(CAGE_file) {
       end = as.numeric(cage_coord_tbl$end)
     )
   )
+
   return(cage_chr_Grange)
 }
 
-#-----------------------------------------
 
-# The input file
-CAGE_feature_in_file <- "./data/feature/feature_in.Rda"
+# The input file, read from the arguments
+input_filepath <- opt$data
 
-# The output file
-out_file <- "./data/feature/feature_wrapped.Rda"
+# Parsing the output path from the provided argument
+output_filepath <- opt$out
 
-# The CAGE file converted to a genomic range object.
-CAGE_feature_wrapped <- build_GRange_fn(CAGE_feature_in_file)
+# Running the function
+wrapped_features <- wrap_features_fn(input_filepath)
 
 # Saving the file to disk.
-save(CAGE_feature_wrapped, file = out_file)
+save(wrapped_features, file = output_filepath)
+
+# Print a success message and the output filepath
+message(green("Successfully wrapped features."))
+message(blue("Data saved to: ", output_filepath))
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # Bootstrapping pipeline
 
-## Generic First sub-Heading
+# Introduction
+This is a bootstrapping pipeline.
+
+# Scripts
+## wrap_features.R 
+The script wraps the input data in a *GenomicRange* object to speed up the calculation of intersections on BED files.
+
+### Inputs:
+- *data* (*d*): the given feature file. Currently accepts a Rda file.
+- *out* (*o*): The file path of the output file.
